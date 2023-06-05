2a2054f989d629bd1212415af5e727a9ab72d0e0
Piper Morgan <jpmac@10-248-184-45.wireless.oregonstate.edu>
2023-06-05 15:17:25 -0700

    first publication

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Hybrid Delta Tracking in MCATK",
+        "excerpt":"Coming soon  ","categories": ["my work"],
+        "tags": ["monte carlo","variance reduction","neutron transport"],
+        "url": "/work/mcatk/",
+        "teaser": null
+      },{
+        "title": "Monte Carlo / Dynamic Code",
+        "excerpt":"Work in progress  ","categories": ["Edge Case"],
+        "tags": ["mcdc","my work"],
+        "url": "/work/mcdc_a/",
+        "teaser": "/assets/images/kobayashi.gif"
+      },{
+        "title": "Therefore",
+        "excerpt":" ","categories": ["my work"],
+        "tags": ["gpu","neutron transport"],
+        "url": "/work/therefore/",
+        "teaser": "/assets/images/reeds.png"
+      },{
+        "title": "Flocking A",
+        "excerpt":"Work in progress  ","categories": ["my work"],
+        "tags": ["trt","variance reduction"],
+        "url": "/work/trt/",
+        "teaser": "/assets/images/trt.gif"
+      }]
